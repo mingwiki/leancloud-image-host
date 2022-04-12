7d2dc987134e32a382b78c894d027cf64dc1f07b
mingwiki <mingwiki@gmail.com>
2022-04-12 15:43:14 +0800

    change API

--- src/models/index.js
+++ src/models/index.js
@@ -2,33 +2,21 @@ import AV, { Query, User } from 'leancloud-storage';
 AV.init({
   appId: "Co2HYYsX3YsrSM8hLn35yMVq-gzGzoHsz",
   appKey: "vpslFNPbTpcTFj4XHIGHP9eH",
-  serverURL: "https://co2hyysx.lc-cn-n1-shared.com"
+  serverURL: "https://api.naizi.fun"
 })
-console.log("AV init...")
+// console.log("AV init...")
 const Auth = {
   register(username, password) {
     let user = new User()
     user.setUsername(username)
     user.setPassword(password)
     return new Promise((resolve, reject) => {
-      user.signUp().then((user) => {
-        console.log(`注册成功。${user}`)
-        resolve(user)
-      }, (error) => {
-        console.log(`注册失败：${error}`)
-        reject(error)
-      })
+      user.signUp().then((user) => resolve(user), (error) => reject(error))
     })
   },
   login(username, password) {
     return new Promise((resolve, reject) => {
-      User.logIn(username, password).then((user) => {
-        console.log(`登录成功。${user}`)
-        resolve(user)
-      }, (error) => {
-        console.log(`登录失败：${error}`)
-        reject(error)
-      });
+      User.logIn(username, password).then((user) => resolve(user), (error) => reject(error));
     })
   },
   logout() {
