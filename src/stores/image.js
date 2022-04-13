05bcbf965be4b4d3ceb4247a2709e9f7746663b2
mingwiki <mingwiki@gmail.com>
2022-04-13 18:41:15 +0800

    单个上传

--- src/stores/image.js
+++ src/stores/image.js
@@ -5,6 +5,7 @@ class ImageStore {
   files = []
   names = []
   imgs = []
+  // uploadFileLists = []
   isUploading = false
 
   constructor() {
@@ -16,24 +17,36 @@ class ImageStore {
   setName(name) {
     this.names.push(name);
   }
+  // setUploadFileLists(file) {
+  //   this.uploadFileLists.push(file);
+  // }
   upload() {
     this.isUploading = true;
-    let promises = [];
-    for (let i = 0; i < this.files.length; i++) {
-      promises.push(Image.upload(this.names[i], this.files[i]));
-    }
+    // let promises = [];
+    // for (let i = 0; i < this.files.length; i++) {
+    //   promises.push(Image.upload(this.names[i], this.files[i]));
+    // }
+    // return new Promise((resolve, reject) => {
+    //   Promise.allSettled(promises).then((result) => {
+    //     result.map((item) => { this.imgs.push(item.value) })
+    //     resolve(this.imgs);
+    //   }, (error) => {
+    //     console.log(error);
+    //     reject(error)
+    //   });
+    //   this.isUploading = false;
+    // })
     return new Promise((resolve, reject) => {
-      Promise.allSettled(promises).then((result) => {
-        result.map((item) => { this.imgs.push(item.value) })
-        resolve(this.imgs);
-      }, (error) => {
-        console.log(error);
-        reject(error)
-      });
+      Image.upload(this.names[this.names.length - 1], this.files[this.files.length - 1])
+        .then((img) => {
+          this.imgs.push(img);
+          resolve(img)
+        }, (error) => {
+          console.log(error)
+          reject(error)
+        })
       this.isUploading = false;
     })
-
   }
 }
-
 export default new ImageStore();
