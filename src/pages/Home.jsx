9bc8d37d84f5aac55770008d23907595f000c58b
mingwiki <mingwiki@gmail.com>
2022-04-14 13:14:18 +0800

    remove useless vars

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React from "react";
 // import context from "../stores/index.jsx";
 import { observer } from "mobx-react";
 import Uploader from "../components/Uploader";
