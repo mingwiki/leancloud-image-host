c10c4657e3e6793771b2258a64bc98e17b9b599c
mingwiki <mingwiki@gmail.com>
2022-04-15 12:39:45 +0800

    Pages Loading Spinning

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,25 @@ import React, { useState, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Loading from "./components/Loading";
-import { ThemeProvider } from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
 import { light, dark, GlobalStyles } from "./theme";
+import { Spin } from "antd";
 
 const Home = React.lazy(() => import("./pages/Home"));
-const Upload = React.lazy(() => import("./pages/Upload"));
 const History = React.lazy(() => import("./pages/History"));
 const About = React.lazy(() => import("./pages/About"));
-const Login= React.lazy(() => import("./pages/Login"));
-const Register= React.lazy(() => import("./pages/Register"));
+const Login = React.lazy(() => import("./pages/Login"));
+const Register = React.lazy(() => import("./pages/Register"));
+
+const CenterSpin = styled(Spin)`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
 
 function App() {
   const [theme, setTheme] = useState({ type: light });
-
   const toggleTheme = () => {
     if (theme.type === light) {
       setTheme({ type: dark });
@@ -28,14 +33,17 @@ function App() {
       <ThemeProvider theme={theme.type}>
         <GlobalStyles />
         <Header toggle={toggleTheme} />
-        <Suspense fallback={<Loading />}>
+        <Suspense
+          fallback={
+            <CenterSpin spinning={true} size="large" tip="页面正在加载中" />
+          }
+        >
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="upload" element={<Upload />} />
             <Route path="history" element={<History />} />
             <Route path="about" element={<About />} />
-            <Route path="login" element={<Login/>} />
-            <Route path="register" element={<Register/>} />
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
           </Routes>
         </Suspense>
         <Footer />
--- src/components/Loading.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-function Component() {
-  return <div>loading...</div>;
-}
-
-export default Component;
